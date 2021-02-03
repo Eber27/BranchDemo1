360d87be87d32fe8c68c5ffaf21011e405d1d28c
Eber27 <eberfreire@gmail.com>
2021-02-02 19:56:36 -0600

    First Branch

--- BranchDemo1/BranchDemo1/Program.cs
+++ BranchDemo1/BranchDemo1/Program.cs
@@ -7,6 +7,7 @@ namespace BranchDemo1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Second branch test");
         }
     }
 }
